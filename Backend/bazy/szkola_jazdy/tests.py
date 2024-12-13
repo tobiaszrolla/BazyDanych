659692df6e471a06a0598b08d9ceddd139cbb05d
tobiaszrolla <154612005+tobiaszrolla@users.noreply.github.com>
2024-12-13 14:16:43 +0100

    dodano możliwość logowania przetestowane

--- Backend/bazy/szkola_jazdy/tests.py
+++ Backend/bazy/szkola_jazdy/tests.py
@@ -1,34 +1,76 @@
 from django.test import TestCase, Client
 import json
-
+from django.contrib.auth import get_user_model
 
 class RegisterTest(TestCase):  # Klasa testowa dziedziczy po TestCase
-    def test_json_post(self):
+    #Metody pomocnicze
+    def register_user(self,
+                      email,
+                      password,
+                      data_urodzenia,
+                      typ_użytkownika,
+                      ):
         client = Client()
         data = {
-            'email': 'test@domena.com',
+            'email': email,
             'nrTelefonu': '123456789',
             'imię': 'User',
             'nazwisko': 'User',
-            'data_urodzenia': '2003-03-03',  # Poprawny format daty
-            'typ_użytkownika': 'kursant',  # Dodano brakujący przecinek
-            'password': 'strong_password',
+            'data_urodzenia': data_urodzenia,
+            'typ_użytkownika': typ_użytkownika,
+            'password': password,
         }
-
-        # Wysyłanie żądania POST
         response = client.post(
-            '/register/',  # Pełna ścieżka URL
+            '/register/',
             data=json.dumps(data),
             content_type='application/json'
         )
-
-        # Sprawdzenie kodu statusu HTTP
         self.assertEqual(response.status_code, 201)
+        return response
 
-        # Sprawdzenie treści odpowiedzi
+    def login_user(self, email, password):
+        client = Client()
+        data = {
+            'email': email,
+            'password': password,
+        }
+        response = client.post(
+            '/login/',
+            data=json.dumps(data),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+        return response
+    def test_register(self):
+        client = Client()
+        response = self.register_user('test@domena.com','strong_password', '2003-03-03','kursant')
         response_data = response.json()
         self.assertIn('message', response_data)
         self.assertEqual(response_data['message'], 'Użytkownik został zarejestrowany pomyślnie!')
-from django.test import TestCase
+        user_from_db = get_user_model().objects.get(email='test@domena.com')
+        print(f'User ID: {user_from_db.id}')
+        print(f'User Email: {user_from_db.email}')
+        print(f'User Name: {user_from_db.imię} {user_from_db.nazwisko}')
+        print(f'User Phone: {user_from_db.nrTelefonu}')
+        print(f'User Type: {user_from_db.typ_użytkownika}')
+        print(f'User Date of Birth: {user_from_db.data_urodzenia}')
+        print(response_data['message'], '\n')
 
-# Create your tests here.
+    def test_login(self):
+        # Rejestracja użytkownika
+        self.register_user('test@domena.com', 'strong_password', '2003-03-03', 'kursant')
+        user_from_db = get_user_model().objects.get(email='test@domena.com')
+        print(f'User ID: {user_from_db.id}')
+        print(f'User Email: {user_from_db.email}')
+        print(f'User Name: {user_from_db.imię} {user_from_db.nazwisko}')
+        print(f'User Phone: {user_from_db.nrTelefonu}')
+        print(f'User Type: {user_from_db.typ_użytkownika}')
+        print(f'User Date of Birth: {user_from_db.data_urodzenia}')
+        # Logowanie użytkownika
+        response = self.login_user('test@domena.com', 'strong_password')
+        response_data = response.json()
+
+        # Sprawdzanie odpowiedzi
+        self.assertIn('message', response_data)
+        self.assertEqual(response_data['message'], 'Zalogowano pomyślnie.')
+        print(response_data['message'], '\n')
--- Backend/bazy/szkola_jazdy/urls.py
+++ Backend/bazy/szkola_jazdy/urls.py
@@ -7,6 +7,6 @@ app_name = 'szkola_jazdy'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/', views.login, name='login'),
     path('register/', views.register, name='register')
 ]
