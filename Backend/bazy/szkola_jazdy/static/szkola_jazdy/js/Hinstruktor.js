a9ef781728bdd157f839968d1e1975929807e7e9
STBD001 <stefanwoj25@gmail.com>
2025-01-05 18:34:26 +0100

    Małe poprawki

--- Backend/bazy/szkola_jazdy/static/szkola_jazdy/js/Hinstruktor.js
+++ Backend/bazy/szkola_jazdy/static/szkola_jazdy/js/Hinstruktor.js
@@ -1,4 +1,8 @@
-function navigateToZajecia() {
-    window.location.href = "XXX.html";
+// Funkcja do przekierowania na stronę dodawania lub usuwania zajęć
+function navigateToZajecia(action) {
+    if (action === 'dodaj') {
+        window.location.href = "dodaj_zajecia.html"; // Przekierowanie na stronę dodawania zajęć
+    } else if (action === 'usun') {
+        window.location.href = "usun_zajecia.html"; // Przekierowanie na stronę usuwania zajęć
+    }
 }
-##
